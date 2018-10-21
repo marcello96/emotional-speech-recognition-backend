2013e09cca43669e97130696d2a7eafb4f5f2130
marcello96 <jakubowskimarcin96@gmail.com>
2018-10-21 16:00:33 +0200

    init mocked service

--- application.py
+++ application.py
@@ -1,11 +1,16 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 
 application = Flask(__name__)
 
 
-@application.route('/analyze')
+@application.route('/analyze', methods=['POST'])
 def analyze_emotion():
-    return 'HAPPY'
+    if request.method == 'POST':
+        data = request.get_json()
+
+        return jsonify({
+            "emotion": "HAPPY"
+        })
 
 
 # run the app.
